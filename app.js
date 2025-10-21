9f71f704cc59ec9b0f5416576e7147d7c22a86fb
Aghakarim-23 <hemidzade1010@gmail.com>
2025-10-22 01:18:34 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const User = require("./models/User")
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const MONGO_URI = process.env.MONGO_URI
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI)
+        console.log("Connected to MONGO DB ATLAS");
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+connectDB()
+
+
+app.post("/register" , async (req,res) => {
+    const {userName, email, password} = req.body;
+
+    try {
+        const newUser = await User.create({
+            userName,
+            email,
+            password
+        })
+        res.status(201).json({message: "User registered successfully", user: newUser})
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Server error" });
+
+    }
+})
+
+app.post("/login" , async (req,res) => {
+    const {email, password} = req.body
+
+    const existingUser = await User.findOne({email})
+
+    if(!existingUser) return res.status(404).json({message: "User not found"})
+
+    if(existingUser) {
+        if(existingUser.password !== password) {
+            return res.status(400).json({message: "Password is wrong"})
+        } 
+        return res.status(201).json({message: "Login succesfully", user: existingUser.userName})
+
+    }
+
+  
+})
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT || 5001, () => {
+    console.log("App is working on 5001");
+})
